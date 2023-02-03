4db61466dcf80d8724e1c817c33d52011c0c6849
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-03 15:13:40 +0100

    Movie functions

--- Movie.php
+++ Movie.php
@@ -7,4 +7,14 @@ class Movie
     public $year;
     public $lenght;
     public $genre;
+
+    
+    public function __construct($_bannerPath, $_title, $_year, $_lenght,  $_genre)
+    {
+        $this->bannerPath = $_bannerPath;
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->lenght = $_lenght;
+        $this->genre = $_genre;
+    }
 }
