0420058906d299f3a5490fd86ac8850f613433e2
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-03 15:19:22 +0100

    Html movies print

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once __DIR__ . './models/Movie.php';
+
+//Create new object
+$kill_bill = new Movie('#', 'Kill Bill: Volume 1', '2003', '1h 51m', 'Azione');
+$shutter_island = new Movie('#', 'Shutter Island', '2010 ', '2h 18m', 'Thriller');
+?>
 
 <head>
     <meta charset="UTF-8">
@@ -8,6 +13,28 @@
     <title>Movies List</title>
 </head>
 <body>
-    
+    <main>
+        <?php
+        echo '<article>';
+        echo  "<img src='{$kill_bill->bannerPath}' alt=''>";
+        echo '<h1>';
+        echo  $kill_bill->title;
+        echo '</h1>';
+        echo '<p>';
+        echo  "Anno: {$kill_bill->year} || Durata: {$kill_bill->lenght} || Genere: {$kill_bill->genre}";
+        echo '</p>';
+        echo '</article>';
+
+        echo '<article>';
+        echo  "<img src='{$shutter_island->bannerPath}' alt=''>";
+        echo '<h1>';
+        echo  $shutter_island->title;
+        echo '</h1>';
+        echo '<p>';
+        echo  "Anno: {$shutter_island->year} || Durata: {$shutter_island->lenght} || Genere: {$shutter_island->genre}";
+        echo '</p>';
+        echo '</article>';
+        ?>
+    </main>
 </body>
 </html>
