0b2c5f05d8cde7181c1d1fa5210ba6cade4e47fd
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-03 17:26:17 +0100

    Index fixes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . './models/Movie.php';
-$kill_bill = new Movie("Kill bill","film","Action","https://i.pinimg.com/736x/50/a6/79/50a67980883a1634f4c458fcf71261e0--kill-bill-movie-movie-covers.jpg","1h 51m","2003");
-$shutter_island = new Movie("Shutter Island","film","Thriller Action","https://i.ytimg.com/vi/Udfq0fiScug/movieposter_en.jpg","2h 18m","2010");
+$kill_bill = new Movie("Kill bill: Vol.1","film","Action","https://i.pinimg.com/736x/50/a6/79/50a67980883a1634f4c458fcf71261e0--kill-bill-movie-movie-covers.jpg","1h 51m","2003");
+$shutter_island = new Movie("Shutter Island","film","Thriller","https://i.ytimg.com/vi/Udfq0fiScug/movieposter_en.jpg","2h 18m","2010");
 ?>
 
 <head>
@@ -30,25 +30,25 @@ $shutter_island = new Movie("Shutter Island","film","Thriller Action","https://i
 
                 <div class="card col-2">
                     <div class="d-flex justify-content-center p-2">
-                        <img class="card-img-top" src="<?php $kill_bill->getCover() ?>" alt="Card image cap">
+                        <img class="card-img-top" src="<?php $kill_bill->getCover(); ?>" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title"><?php $kill_bill->getTitle() ?></h5>
-                        <h5 class="card-text"><?php $kill_bill->getTypeMovie() ?></h5>
-                        <h5 class="card-text"><?php $kill_bill->getYear() ?></h5>
-                        <h5 class="card-text"><?php $kill_bill->getLenght() ?></h5>
+                        <h5 class="card-title"><?php $kill_bill->getTitle(); ?></h5>
+                        <h5 class="card-text"><?php $kill_bill->getTypeMovie(); ?></h5>
+                        <h5 class="card-text"><?php $kill_bill->getYear(); ?></h5>
+                        <h5 class="card-text"><?php $kill_bill->getLenght(); ?></h5>
                     </div>
                 </div>
 
                 <div class="card col-2">
                     <div class="d-flex justify-content-center p-2">
-                        <img class="card-img-top" src="<?php $shutter_island->getCover() ?>" alt="Card image cap">
+                        <img class="card-img-top" src="<?php $shutter_island->getCover(); ?>" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title"><?php $shutter_island->getTitle() ?></h5>
-                        <h5 class="card-text"><?php $shutter_island->getTypeMovie() ?></h5>
-                        <h5 class="card-text"><?php $shutter_island->getYear() ?></h5>
-                        <h5 class="card-text"><?php $shutter_island->getLenght() ?></h5>
+                        <h5 class="card-title"><?php $shutter_island->getTitle(); ?></h5>
+                        <h5 class="card-text"><?php $shutter_island->getTypeMovie(); ?></h5>
+                        <h5 class="card-text"><?php $shutter_island->getYear(); ?></h5>
+                        <h5 class="card-text"><?php $shutter_island->getLenght(); ?></h5>
                     </div>
                 </div>
             </div>
